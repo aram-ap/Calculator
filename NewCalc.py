14bc54b2d96a1c4f23d36d61230eafd22b553924
Aram Aprahamian <aramap22@gmail.com>
2023-12-02 01:10:24 -0800

    Finished calculator in 'NewCalc.py', removed some unnecessary complexity

--- NewCalc.py
+++ NewCalc.py
@@ -90,132 +90,122 @@ class RoundedButton(tk.Canvas):
         else:
             self.itemconfig(self.rect, fill=self.btnbackground)
 
+def is_operator(case):
+    if(case in ["+","-","*","/"]):
+       return True
+    return False
 
-class Calculator:
+need_reset=False
 
-    def __init__(self):
-        self._val = 0
-        self._actions = []
-        pass
+def calculator_text_enter(code, textlabel):
+    global need_reset
+    if(need_reset and not is_operator(code)):
+        textlabel.configure(text="")
+    need_reset=False
 
-    def get_result_value(self):
-        return self._val
-
-    def set_actions_by_string(self, param):
-        param.replace(" ", "")
-        self._actions = param.split(",")
-        return param
-
-
-
-    def string_to_num(self, str):
-        return float(str)
-
-    def clear_all(self):
-        self._val = 0
-        self._actions = []
-
-    def do_calculation(self, case, num):
-        temp = self._val
-
-        match case:
-            case "+":  # ADDITION
-                self._val += num
-            case "-":  # SUBTRACTION
-                self._val -= num
-            case "*":  # MULTIPLICATION
-                self._val *= num
-            case "/":  # DIVISION
-                if (num == 0):
-                    self.val = -1
-                else:
-                    self._val /= num
-
-    def execute_calculation(self, str):
-        self._actions = self.set_actions_by_string(str)
-
-        for i in range(len(self._actions)):
-            item = self._actions[i]
-
-            if (item.isnumeric()) and i == 0:
-                self._val += self.string_to_num(item)
-
-            else:
-                if (i + 1 in range(len(self._actions))):
-                    self.do_calculation(item, self.string_to_num(self._actions[i + 1]))
-                else:
-                    self.do_calculation(item, 0)
-
-
-def command(d):
-    print(d)
+    value = textlabel.cget("text") 
+    match str(code):
+        case "ENTER":
+            need_reset = True
+            try:
+                textlabel.configure(text=eval(value))
+            except ZeroDivisionError as e:
+                textlabel.configure(text="--ERROR--\nDIVIDE BY ZERO")
+        case "CLR":             
+            need_reset = False
+            textlabel.configure(text="")
+        case _:
+            value = f"{value}{code}" 
+            textlabel.configure(text=value)
+             
+    print(code)
 
 def main():
-## CALCULATOR INIT
-    c = Calculator()
-
 ## CONFIG INIT
-    frame_width = 500
 
-    num_width = 7
-    num_height = num_width
-    num_borderwidth = 1
-    num_bordersize = 100
+    
+    _size_configs = {
+        "frame_width"       : 410,
+        "frame_height"      : 460,
+        "num_width"         : 6,
+        "num_height"        : 3,
+        "num_bordersize"    : 100,
+        "num_borderwidth"   : 2,
+        "num_font_size"     : 12,
+        "display_font_size" : 18
 
-    frame_color         = "white"
+    }
+
+
+
+    frame_color         = "#283A44"
     display_color       = "black"
     display_text_color  = "white"
     num_color           = "white"
     num_decimal_color   = "white"
-    operator_color      = "#283A44"
-    enter_button_color  = "#CFEBF3"
+    operator_color      = "#5F94A4"
+    enter_button_color  = "#1D1B2E"
+
     clear_button_color  = "#1D1B2E"
 
-    ttk.Style().configure('operators', background="#283A44")
 
-
+    
 ## GUI INIT
 
     root = tk.Tk()
     root.resizable(False,False)
-    root.geometry(f"{frame_width}x600")
+    root.geometry(f"{_size_configs["frame_width"]}x{_size_configs["frame_height"]}")
 
+    
 
-    display_frame = tk.Frame(root, height=100, width=frame_width, bg=display_color)
+    display_frame = tk.Frame(root, height=100, width=_size_configs["frame_width"], bg=display_color)
     display_frame.pack_propagate(False)
     display_frame.pack(fill=tk.X)
 
-    display_label = tk.Label(display_frame,anchor=tk.CENTER, borderwidth=0, fg=display_text_color)
+    label_text = tk.StringVar()
+    label_text.set("")
+    display_label = tk.Label(display_frame,
+                             anchor=tk.CENTER, 
+                             borderwidth=0, 
+                             fg=display_text_color, 
+                             bg=display_color, 
+                             text=label_text.get(), 
+                             font=("Helvetica", _size_configs["display_font_size"]))
+    display_label.pack_propagate(False)
+    display_label.pack(fill=tk.BOTH)
 
-    keypad_frame = tk.Frame(root, height=500, width=frame_width)
-    # keypad_frame.grid_propagate(False)
+    keypad_frame = tk.Frame(root, height=_size_configs["frame_height"], width=_size_configs["frame_width"])
     keypad_frame.pack_propagate(False)
     keypad_frame.pack()
 
     keypad_layout = [
-        [7, 8, 9, "+", "ENTER"],
+        [7, 8, 9, "+", "CLR"],
         [4, 5, 6, "-"],
-        [1, 2, 3, "*", "CLR"],
+        [1, 2, 3, "*", "ENTER"],
         [0, -1, ".", "/"]
     ]
 
     keypad_buttons = []
+
 
     for i in range(len(keypad_layout)):
         temp = {}
         for j in range(len(keypad_layout[i])):
             if keypad_layout[i][j] == -1:
                 continue
-
+            
+            buttontext = str(keypad_layout[i][j])
             button = (tk.Button(keypad_frame,
-                                text=keypad_layout[i][j],
-                                width=num_width,
-                                height=num_height,
+                                text=buttontext,
+                                width=_size_configs["num_width"],
+                                height=_size_configs["num_height"],
                                 bg=num_color,
-                                bd=num_borderwidth,
-                                command=lambda j=keypad_layout[i][j]: command(j)
+                                bd=_size_configs["num_borderwidth"],
+                                command=lambda j=buttontext: calculator_text_enter(j, display_label)
                                 ))
-            temp[str(keypad_layout[i][j])] = button
+            button.config(font=("Helvetica",16))
+            temp[str(buttontext)] = button
+
         keypad_buttons.append(temp)
 
     for i in range(len(keypad_layout)):
@@ -225,77 +215,25 @@ def main():
             key = str(keypad_layout[i][j])
             keypad_buttons[i][key].grid(row=i, column=j)
 
+    for i in range(len(keypad_layout)):
+        keypad_buttons[i].get(keypad_layout[i][3]).configure(bg=operator_color)
+
     print(keypad_buttons[3][str(0)])
 
     button_zero = keypad_buttons[3].get("0")
-    button_enter = keypad_buttons[0].get("ENTER")
-    button_clr = keypad_buttons[2].get("CLR")
-
+    button_enter = keypad_buttons[2].get("ENTER")
+    button_clr = keypad_buttons[0].get("CLR")
+    button_decimal = keypad_buttons[3].get(".")
 
     button_zero.grid(row=3,column=0,columnspan=2, sticky="nsew")
-    button_enter.grid(row=0, column=4, rowspan=2, sticky="nsew")
-    button_clr.grid(row=2,column=4, rowspan=2, sticky="nsew")
+    button_enter.grid(row=2, column=4, rowspan=2, sticky="nsew")
+    button_clr.grid(row=0,column=4, rowspan=2, sticky="nsew")
+        
+    button_enter.configure(bg=enter_button_color, fg="white")
+    button_clr.configure(bg=clear_button_color, fg="white")
+    button_decimal.configure(bg=num_decimal_color)
 
-    button_enter.configure(highlightbackground=operator_color)
-    button_clr.configure(highlightbackground=operator_color)
-
-    # keypad_buttons[3][button_decimal_key].grid(row=3,column=2, sticky="nsew")
     root.mainloop()
-    # main frame
-
-    # frame = tk.Frame(root, width=50,height=700, background="blue")
-    # frame.pack_propagate(False)
-
-
-
-
-
-
-    # frame.pack()
-    #
-    # digit_box = tk.Frame(frame, background="black", width=50,height=50)
-    # digit_box.pack_propagate(False)
-    # digit_box.pack()
-    #
-    # textBox = tk.Label(digit_box,text="1+1",background="black", font=10,fg='white')
-    # textBox.grid(row=0,column=0)
-    # textBox.pack_propagate(False)
-    # textBox.pack()
-    #
-    # btn_seven = tk.Button(frame, text="7", width=5, background="white")
-    # btn_seven.grid(row=1, column=0, padx=5, pady=10)
-    # btn_seven.pack(fill="both")
-    #
-    #
-    # btn_eight = tk.Button(frame, text="8", width=5, background="white")
-    # btn_eight.grid(row=1, column=1, padx=5, pady=10)
-    # btn_eight.pack(fill="both")
-    #
-    # btn_nine = tk.Button(frame, text="9", width=5, background="white")
-    # btn_nine.grid(row=1, column=2, padx=5, pady=10)
-    # btn_nine.pack(fill="both")
-    #
-    # btn_four= tk.Button(frame,text="4", width=5, background="white")
-    # btn_four.grid(row=2,column=0, padx=5, pady=10)
-    # btn_four.pack(fill="both")
-    #
-    # btn_five= tk.Button(frame,text="5", width=5, background="white")
-    # btn_five.grid(row=2,column=1, padx=5, pady=10)
-    # btn_five.pack(fill="both")
-    #
-    # btn_six = tk.Button(frame,text="6", width=5, background="white")
-    # btn_six.grid(row=2,column=2, padx=5, pady=10)
-    # btn_six.pack(fill="both")
-    #
-    #
-    #
-    # btn = RoundedButton(frame,text="Button", radius=100,btnbackground="gray", btnforeground="white", clicked=func)
-    # btn.grid(row=1,column=0)
-    # btn.pack(expand=False,fill="both")
-
-    # btn = RoundedButton(text="This is a \n rounded button", radius=1000, btnbackground="#0078ff", btnforeground="#ffffff", clicked=func)
-    # btn.pack(expand=True, fill="both")
-
-
+ 
 if __name__ == "__main__":
     main()
